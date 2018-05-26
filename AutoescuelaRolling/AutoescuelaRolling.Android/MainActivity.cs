7d409b10ad71f55a5b663843e7afbfeed9645d05
alvaromad <camarero_acb@hotmail.com>
2018-05-26 20:23:19 +0200

    Splashscreen

--- AutoescuelaRolling/AutoescuelaRolling.Android/MainActivity.cs
+++ AutoescuelaRolling/AutoescuelaRolling.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace AutoescuelaRolling.Droid
 {
-    [Activity(Label = "AutoescuelaRolling", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "AutoescuelaRolling", Icon = "@mipmap/icon", Theme = "@style/Theme.Splash", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
